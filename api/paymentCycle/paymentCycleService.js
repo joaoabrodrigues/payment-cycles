eca637119870647c2c72f562c7e3d6e4a69b74db
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-05-23 22:48:59 -0300

    Creating count service

--- api/paymentCycle/paymentCycleService.js
+++ api/paymentCycle/paymentCycleService.js
@@ -3,4 +3,14 @@ const paymentCycle = require('./paymentCycle')
 paymentCycle.methods(['get', 'post', 'put', 'delete'])
 paymentCycle.updateOptions({ new: true, runValidators: true })
 
+paymentCycle.route('count', function(req, res, next) {
+  paymentCycle.count(function(error, value) {
+    if (error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
 module.exports = paymentCycle
