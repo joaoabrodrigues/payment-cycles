35b1f67ca06279eb7c894c629858a43520dd07ea
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-05-25 21:25:35 -0300

    Improving error messages

--- backend/api/paymentCycle/paymentCycleService.js
+++ backend/api/paymentCycle/paymentCycleService.js
@@ -1,7 +1,27 @@
 const paymentCycle = require('./paymentCycle')
+const _ = require('lodash')
 
 paymentCycle.methods(['get', 'post', 'put', 'delete'])
 paymentCycle.updateOptions({ new: true, runValidators: true })
+
+paymentCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+  const bundle = res.locals.bundle
+
+  if (bundle.errors) {
+    var errors = parseErros(bundle.errors)
+    res.status(500).json({errors})
+  } else {
+    next()
+  }
+}
+
+function parseErros(nodeRestfulErrors) {
+  const errors = []
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  return errors
+}
 
 paymentCycle.route('count', function(req, res, next) {
   paymentCycle.count(function(error, value) {
