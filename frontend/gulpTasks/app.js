33db88b2bc40777f87dc86a3a12e2eb7bfe14aaf
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-05-29 20:26:13 -0300

    Finishing build configuration

--- frontend/gulpTasks/app.js
+++ frontend/gulpTasks/app.js
@@ -1,4 +1,10 @@
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const uglifycss = require('gulp-uglifycss')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
+
 
 gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
 
@@ -9,13 +15,21 @@ gulp.task('app.html', function(){
 })
 
 gulp.task('app.css', function(){
-
+  gulp.src('app/**/*.css')
+    .pipe(uglifycss({ "uglyComments": true }))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('app.js', function(){
-
+  gulp.src('app/**/*.js')
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('public/assets/js'))
 })
 
 gulp.task('app.assets', function(){
-
+  gulp.src('assets/**/*.*')
+    .pipe(gulp.dest('public/assets'))
 })
