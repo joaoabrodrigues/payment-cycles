523d3e243a242b591c0f6ea823cd08dda241e926
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-06-05 20:07:37 -0300

    Using angular 1.6 method on dashboard controller

--- frontend/app/dashboard/dashboardController.js
+++ frontend/app/dashboard/dashboardController.js
@@ -8,7 +8,7 @@ function DashboardController($scope, $http) {
 
   $scope.getSummary = function() {
     const url = 'http://localhost:3003/v1/api/paymentSummary'
-    $http.get(url).success(function({credit = 0, debt = 0}){
+    $http.get(url).then(function({credit = 0, debt = 0}){
       $scope.credit = credit
       $scope.debt = debt
       $scope.total = credit - debt
