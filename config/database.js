24df454103b7dc67d66a52dd1f3c70afdd048596
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-07-31 19:01:26 -0300

    Fixing connection string url and run script

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+module.exports = mongoose.connect('mongodb://develop:gLUFZfAQt08UNQWO@cluster0-shard-00-00-kuksm.mongodb.net:27017,cluster0-shard-00-01-kuksm.mongodb.net:27017,cluster0-shard-00-02-kuksm.mongodb.net:27017/db_finnance?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 mongoose.Error.messages.general.required = "Field '{PATH}' is mandatory."
 mongoose.Error.messages.Number.max = "Value '{VALUE}' is greater than maximum allowed ({MAX})."
