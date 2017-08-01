567b3a933c6fccad4c6ffe32e6ead9b709c5ac66
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-07-31 21:41:37 -0300

    Fixing database name

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://develop:gLUFZfAQt08UNQWO@cluster0-shard-00-00-kuksm.mongodb.net:27017,cluster0-shard-00-01-kuksm.mongodb.net:27017,cluster0-shard-00-02-kuksm.mongodb.net:27017/db_finnance?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
+module.exports = mongoose.connect('mongodb://develop:gLUFZfAQt08UNQWO@cluster0-shard-00-00-kuksm.mongodb.net:27017,cluster0-shard-00-01-kuksm.mongodb.net:27017,cluster0-shard-00-02-kuksm.mongodb.net:27017/db_finance?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 mongoose.Error.messages.general.required = "Field '{PATH}' is mandatory."
 mongoose.Error.messages.Number.max = "Value '{VALUE}' is greater than maximum allowed ({MAX})."
