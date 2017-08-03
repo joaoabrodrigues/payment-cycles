97321995e799306d70924f9eff512dc70e6a0819
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-08-02 23:10:22 -0300

    Adding date on debts and credits

--- api/paymentCycle/paymentCycle.js
+++ api/paymentCycle/paymentCycle.js
@@ -3,14 +3,17 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
+  value: { type: Number, min: 0, required: true },
+  date: { type: String, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
   value: { type: Number, min: 0, required: true },
   status: { type: String, required: false, uppercase: true,
-    enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+    enum: ['PAGO', 'PENDENTE', 'AGENDADO']
+  },
+  date: { type: String, required: true }
 })
 
 const paymentCycleSchema = new mongoose.Schema({
